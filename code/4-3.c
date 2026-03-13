8f414b5d1c817df7ea9e21e8014483b7eb1d3348
hyerin9031 <148173524+hyerin9031@users.noreply.github.com>
2026-03-13 11:02:47 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,32 @@
 
 int main()
 {
+    int a, result=1;
+    scanf("%d", &a);
 
+    if(a<=1){
+        result=0;
+    }
+    else{
+        for(int i=2;i<a/2;i++){
+            if(a%i==0){
+                result = 0;
+                break;
+            }
+            else{
+                continue;
+            }
+        }
+    }
+    
+
+    if(result){
+        printf("true");
+    }
+    else{
+        printf("false");
+    }
+    
     return 0;
 }
 
