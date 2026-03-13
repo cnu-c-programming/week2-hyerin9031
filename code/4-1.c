e03f7a0c7a8b794dc493afbb8fce97150c60cab7
hyerin9031 <148173524+hyerin9031@users.noreply.github.com>
2026-03-13 10:30:27 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,26 @@
 
 int main()
 {
+    int a, b;
+    char c;
+    int result;
 
+    scanf("%d %d %c", &a, &b, &c);
+    if(c == '+'){
+        result = a+b;
+    }
+    else if(c == '-'){
+        result = a-b;
+    }
+    else if(c == '*'){
+        result = a*b;
+    }
+    else{
+        result = a/b;
+    }
+
+    printf("%d", result);
+    
     return 0;
 }
 
